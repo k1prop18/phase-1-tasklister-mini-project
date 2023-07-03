e9f3139f55a44b1a64b48991ba4e774801a7f9b8
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-07-03 10:20:49 +0300

    Lab done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  let addTask = document.getElementById('create-task-form')
+  console.log(addTask)
+  addTask.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    onsubmit(e.target.new_task.value)
+  })
 });
+function onSubmit(todo) {
+  let p = document.createElement("p");
+  console.log(p);
+  p.textContent = `${todo}`;
+  let btn = document.createElement("button");
+  btn.textContent = "Delete Task";
+  btn.addEventListener("click", deleteTasks);
+  p.appendChild(btn);
+  console.log(btn);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function deleteTasks(e) {
+  e.target.parentNode.remove();
+}
